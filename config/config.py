1547cda7da5ae5b5ae5782a152181bdecd15eb0d
yanzhenxing123 <209243446@qq.com>
2022-06-26 23:14:47 +0800

    feat: config.py

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ import utils
 
 base_path = utils.get_project_path()
 
-with open(base_path + "\\config\\config.yaml", encoding="utf-8") as file:
+with open(base_path + "/config/config.yaml", encoding="utf-8") as file:
     Config = yaml.full_load(file)
     MONGO = Config.get('Mongo')
     Kafka = Config.get('Kafka')
