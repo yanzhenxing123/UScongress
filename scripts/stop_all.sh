a5f68311238c748caf1e740fb930ccaa14d7def4
yanzhenxing123 <209243446@qq.com>
2022-07-01 14:12:57 +0800

    sh

--- scripts/stop_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /root/Files/UScongress
-bash /root/Files/UScongress/scripts/kill_app.sh
-bash /root/Files/UScongress/scripts/kill_crawl_bill.sh
-bash /root/Files/UScongress/scripts/kill_chrome.sh
-
---
+++ stop_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+bash scripts/kill_app.sh
+bash scripts/kill_chrome.sh
+bash scripts/kill_crawl_bill.sh
