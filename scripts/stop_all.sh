452fbc010eb70f2a2e2a4eb3ec3e443871418eb4
yanzhenxing123 <209243446@qq.com>
2022-07-01 14:06:14 +0800

    fix: sh bug

--- scripts/stop_all.sh
+++ scripts/stop_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /root/Files/UScongress
-bash kill_app.sh
-bash kill_crawl_bill.sh
-bash kill_chrome.sh
+bash /root/Files/UScongress/kill_app.sh
+bash /root/Files/UScongress/kill_crawl_bill.sh
+bash /root/Files/UScongress/kill_chrome.sh
